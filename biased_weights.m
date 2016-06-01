9c61ad68dd70bbf1122d689cab779cdb2e26d6bb
lingya09731425 <lingya09731425@gmail.com>
2016-06-01 15:23:55 +0200

    add rate-based simulation

---
+++ biased_weights.m
@@ -0,0 +1,24 @@
+function W = biased_weights(N_in, bias)
+    W = zeros(N_in);
+
+    for i = 0 : N_in - 1
+        for j = 1 : N_in - i
+            W(j,j+i) = (~ i == 0) * normpdf(i, 0, 2);
+            W(j+i,j) = (~ i == 0) * normpdf(i, 0, 2);
+        end
+    end
+
+    W = W / normpdf(0, 0, 2);
+    W = bias * W + rand(N_in) / 20 - mean(mean(bias * W));
+    W(W < 0) = 0;
+    
+%     figure;
+%     colormap('hot');
+%     imagesc(W);
+%     colorbar; caxis([0,0.2]);
+%     getframe;
+end
+
+
+
+
---
+++ main_script_rates.m
@@ -0,0 +1,51 @@
+clear; close all;
+
+% independent poisson events
+indp = true;
+
+% randomness
+biased_W = true; bias = 0.02;
+random_L_events = true;
+
+% plot each step
+plot_W_all = true;
+
+% number of cells
+N_in = 50;
+N_out = 50;
+
+% time resolution
+total_ms = 1000;
+dt_per_ms = 1000;
+
+% time constants
+tau_w = 250;
+tau_out = 0.01;
+tau_theta = 0.5;
+
+% thresholds and learn rate
+out_thres = 0.5;
+W_thres = 0.15;
+
+% parameters for events
+L_portion = 0.7; H_portion = 1 - L_portion;
+L_dur = 0.35; L_pct = [0.2 0.6];
+H_dur = 0.07; H_pct = [0.8 1.0];
+
+% if events are independent
+L_avg_period = 1;
+H_avg_period = 3;
+
+%% independent
+
+filename = sprintf('Jun1/independent_rates_Ld%d_Hd%d_Wthr%d_outthr%d_%s.png', ...
+    L_dur * 1000, H_dur * 1000, ...
+    W_thres * 100, out_thres * 100, ...
+    datestr(now, 'HHMM'));
+
+independent_rates(biased_W, bias, random_L_events, N_in, N_out, total_ms, dt_per_ms, ...
+    out_thres, W_thres, L_avg_period, H_avg_period, ...
+    L_dur, H_dur, L_pct, H_pct, ...
+    tau_w, tau_out, tau_theta, ...
+    filename, plot_W_all)
+
